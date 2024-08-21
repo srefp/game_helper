705b2660ddb3abd51d761011754bdeb9606d2347
liukangjian <liukangjian@bsmail.com>
2024-08-21 21:15:41 +0800

    新路线

--- autohotkey/text_route/小怪丨原水传奇.lua
+++ autohotkey/text_route/小怪丨原水传奇.lua
@@ -1,9 +1,9 @@
 monster: [10, 1], pos: [31828, 36746], narrow: -1
 monster: [9, 3], pos: [28208, 47982]
 monster: [10, 2], pos: [56827, 49500], try: 2
+monster: [10, 2], pos: [51943, 45188]
 monster: [10, 2], pos: [42791, 34620]
 monster: [9, 2], pos: [29540, 16642]
 monster: [9, 2], pos: [29540, 16642]
-monster: [9, 1], pos: [18715, 52112]
-monster: [9, 1], pos: [39922, 27878]
 monster: [9, 2], pos: [49587, 21440]
+monster: [9, 1], pos: [39922, 27878]
